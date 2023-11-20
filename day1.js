824f1cc554e9a6bb581f3b790ed6d6ce9ad772a9
Kidus Hailu <kidustheastronomer@gmail.com>
2023-11-20 22:57:58 +0300

    ALL is good

--- day1.js
+++ day1.js
@@ -2,3 +2,25 @@ const speedLimit;
 var pace;
 String Named;
 let age;
+
+let firstName = 'Kidus'
+let LastName = 'Hailu'
+let age = '20'
+const Gender = 'Non-Binary;)'
+
+
+let firstName = 'Kidus'
+let lastName = 'Hailu'
+let country = 'Ethiopia'
+let city = 'Addis BABA'
+let age = 20
+let isMarried = false
+
+let name = 'Kidus',
+    age = '20',
+    country = 'Ethiopia',
+    isMarried = false
+
+let myAge = '20'
+let yourAge = '21'
+console.log('My age is ' + myAge + '\n' + 'Your age is ' + yourAge)
