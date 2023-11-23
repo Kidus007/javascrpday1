eb8c85daf6182aeb226bc8b0936c77b2df3dedd1
Kidus Hailu <kidustheastronomer@gmail.com>
2023-11-23 08:29:08 +0300

    assignmet

---
+++ day2.js
@@ -0,0 +1,14 @@
+let strw = 'You cannot end a sentence with because because because is a conjunction'
+console.log(strw.lastIndexOf('because'))
+
+let word = '30 Days Of JavaScript'
+console.log(word.repeat(2))
+
+console.log(word.substring(0, 2))
+
+String word = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+list = word.split(', ')
+
+Mod = word.replace('JavaScript', 'Python')
+
+console.log(word)
